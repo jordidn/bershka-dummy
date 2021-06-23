ca01f5f039602b4a72b097803478d4b5f16fa37e
Jordi Duran <jduran@bemobile.es>
2021-06-23 13:08:59 +0200

    Fix newsletter json

--- bershka-dummy/server.js
+++ bershka-dummy/server.js
@@ -11,18 +11,29 @@ var PORT = 3000;
 
 // MARK: - Middleware
 app.use(middleware.logger);
-app.get('/*', function (req, res) {
-	console.log('res:' + res)
 
-	var path = __dirname + '/static' + req.path + '.json';
+app.get('/content-cms/forms/*', function (req, res) {
+	function removeStore(the_url) {
+		var the_arr = the_url.split('/');
+		the_arr.pop();
+		return( the_arr.join('/') );
+	}
+
+	var relativePath = removeStore(req.path)
+	var path = __dirname + '/static' + relativePath + '.json';
 	fs.readFile(path, 'utf8', function (err, data) {
 		if (err) throw err;
 		res.send(JSON.parse(data));
 	});
 });
 
-
-
+app.get('/*', function (req, res) {
+	var path = __dirname + '/static' + req.path + '.json';
+	fs.readFile(path, 'utf8', function (err, data) {
+		if (err) res.status(409).send({ message: 'File not found' });
+		res.send(JSON.parse(data));
+	});
+});
 
 
 // MARK: - Listen port
